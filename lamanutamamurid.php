fd06283978a3d6477ff05da9cdd8196d1e549341
weipinang <weipin0508@gmail.com>
2021-04-03 00:54:22 +0800

    upload

--- lamanutamamurid.php
+++ lamanutamamurid.php
@@ -11,7 +11,7 @@ $idpengguna=$_SESSION['idpengguna'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="stylesheet" href="./lamanutamastyle.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="./lamanutamamuridstyle.css?v=<?php echo time(); ?>">
     <title>Laman Utama</title>
 </head>
 <body>
--- proseslogin.php
+++ proseslogin.php
@@ -18,7 +18,25 @@
             $_SESSION['idpengguna']=$row['idpengguna'];
             //akan bawa ke menu.php
             $_SESSION['level']=$row['peranan'];
-            header ("Location: lamanutama.php");
+            header ("Location: lamanutamamurid.php");
+        }
+    }
+
+    if (isset($_POST['idpengguna'])){
+        $idpengguna = $_POST['idpengguna'];
+        $katalaluan = $_POST['katalaluan'];
+        //nama mesti sama dengan $conn di connect.php
+        $query = mysqli_query($conn, "SELECT* FROM pengguna WHERE idpengguna='$idpengguna' AND katalaluan='$katalaluan' AND idpengguna LIKE 'G%'");
+        $row = mysqli_fetch_assoc($query);
+        //perlu sama dengan post
+        if (mysqli_num_rows($query)==0||$row['katalaluan']!=$katalaluan){
+            echo"<script>alert('Minta maaf, ID Pengguna atau kata laluan anda salah.');
+            window.location='login.php'</script>";
+        }else{
+            $_SESSION['idpengguna']=$row['idpengguna'];
+            //akan bawa ke menu.php
+            $_SESSION['level']=$row['peranan'];
+            header ("Location: lamanutamaguru.php");
         }
     }
 ?>
