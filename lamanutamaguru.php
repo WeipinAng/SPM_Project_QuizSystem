359cc927a101da1477c509d7aedbd8dbaed2ba0b
weipinang <weipin0508@gmail.com>
2021-04-03 01:03:22 +0800

    upload

--- lamanutamaguru.php
+++ lamanutamaguru.php
@@ -25,7 +25,7 @@ $idpengguna=$_SESSION['idpengguna'];
             <ul>
                 <li><a href="#lamanutama.php"><i class="fas fa-info-circle"></i>Profil</a></li>
                 <li><a href="#koleksikuiz.php"><i class="fab fa-wpforms"></i>Koleksi Kuiz</a></li>
-                <li><a href="#prestasikuiz.php"><i class="fas fa-table"></i>Prestasi Kuiz</a></li>
+                <li><a href="#prestasikuiz.php"><i class="fas fa-table"></i>Data Prestasi Kuiz Murid</a></li>
             </ul>
         </div>
         <div class="space">
