055793d9ff821bb4b26b22fde3b7fef669fc65e4
weipinang <weipin0508@gmail.com>
2021-06-26 22:05:06 +0800

    upload

--- importkuiz.php
+++ importkuiz.php
@@ -6,7 +6,7 @@ $idpengguna=$_SESSION['idpengguna'];
 ?>
 
 <head>
-    <link rel="stylesheet" href="importkuiz.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/importkuiz.css?v=<?php echo time(); ?>">
     <title>Laman Utama</title>
 </head>
     <!-- header mula -->
@@ -17,16 +17,46 @@ $idpengguna=$_SESSION['idpengguna'];
             </div>
     <!-- header tamat -->
             <div class="maincontent">
-                <div class="title">IMPORT KUIZ</div>
+                <div class="title">IMPORT NAMA PELAJAR DARI FAIL CSV</div>
                 <div class="separator"></div>
                 <div class="detailbox">
                     <h3>
                     <form class="uploadform" action="prosesimportkuiz.php" method="post" name="upload_excel" enctype="multipart/form-data">
                         <div class="forminput">
-                            <p>Pilih lokasi fail dalam bentuk .CSV/Excel</p>
+                            <p style="font-size: 14px">Kemudahan untuk mendaftar nama murid secara berkelompok</p><br>
+                            <p style="font-size: 14px">Pilih lokasi fail dalam bentuk CSV/Excel:</p>
                         </div>
-                        <input class="browse" type="file" name="file" id="file" class="input-large" placeholder="browse"><br>
-                        <button class="upload" type="submit" id="submit" name="import">Muat Naik</button>
+                        <!-- Memanggil Fail CSV untuk melaksanakan arahan IMPORT -->
+                        <form action="importcsv.php" method="post" name="daftarmurid" enctype="multipart/form-data">
+                            <input type="file" id="actual-btn" hidden/>
+                            <label for="actual-btn">Pilih Fail CSV</label>
+                            <span id="filechosen">No file chosen</span>
+                            <script>
+                                const actualBtn = document.getElementById('actual-btn');
+                                const fileChosen = document.getElementById('filechosen');
+                                actualBtn.addEventListener('change', function(){
+                                    fileChosen.textContent = this.files[0].name
+                                })
+                            </script>
+                            <button class="submit" type="submit" id="submit" name="import">Muat Naik</button>
+                        </form>
+                        <br>
+                        <p style="font-size: 13px">Sila mencipta fail dalam Microsoft Excel dan simpan fail dalam bentuk CSV mengikut aturan lajur seperti di bawah:</p>
+                        <br><br>
+                        <table class="import" autowidth="false">
+                            <tr style="font-size: 14px">
+                                <th style="width: 30%;">Nama Murid</th>
+                                <th style="width: 15%;">ID Pengguna</th>
+                                <th style="width: 30%;">Kata Laluan</th>
+                                <th style="width: 25%;">No. Telefon</th>
+                            </tr>
+                            <tr style="font-size: 14px">
+                                <td>Abdul Rahim bin Ahmad</td>
+                                <td>M0001</td>
+                                <td>rahim2004</td>
+                                <td>0193784789</td>
+                            </tr>
+                        </table>
                     </form>                           
                     </h3>
                 </div>                 
