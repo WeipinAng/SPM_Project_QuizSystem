4f56a6498021a3d1a60530bb9b0a689063160b94
weipinang <weipin0508@gmail.com>
2021-10-07 18:56:51 +0800

    upload

--- hapuskantopik.php
+++ hapuskantopik.php
@@ -11,12 +11,7 @@ $deletesoalan = mysqli_query($conn,"DELETE FROM soalan WHERE idtopik='$idtopik'"
 $queryidsoal = mysqli_query($conn, "SELECT * FROM soalan WHERE idtopik='$idtopik'");
 while ($datasoal = mysqli_fetch_array($queryidsoal)) {
     $idsoal = $datasoal['idsoal'];
-    echo"<script>alert('$idsoal')</script>";
-    $queryidpilihan = mysqli_query($conn, "SELECT * FROM pilihan WHERE idsoal='$idsoal'");
-    while ($datapilihan = mysqli_fetch_array($queryidpilihan)) {
-        $idpilihan = $datapilihan['idpilihan'];
-        $deletepilihan = mysqli_query($conn, "DELETE FROM pilihan WHERE idpilihan='$idpilihan'");
-    }
+    $deletepilihan = mysqli_query($conn, "DELETE FROM pilihan WHERE idsoal='$idsoal'");
 }
 
 $deleteperekodan = mysqli_query($conn,"DELETE FROM perekodan WHERE idtopik='$idtopik'");
