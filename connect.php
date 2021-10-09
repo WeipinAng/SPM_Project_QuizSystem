830212097f023cd45f6eb14eaa10c3078c2ef16f
weipinang <weipin0508@gmail.com>
2021-10-09 23:27:51 +0800

    upload

--- connect.php
+++ connect.php
@@ -1,10 +1,19 @@
 <?php
+//connect.php
+//menyambung fail php dengan pangkalan data
 $host="localhost";
+
+//nama pengguna bagi phpMySQL
 $user="root";
+
+//kata laluan bagi MySQL xampp ialah null (kosongkan)
 $password="";
+
+//nama pangkalan data
 $database="perekodan";
 
-//perlu sama dengan nama variable di atas
+//umpukkan semua maklumat ke pemboleh ubah conn
+//perlu sama dengan nama pemboleh ubah di atas
 $conn=mysqli_connect($host,$user,$password,$database);
 if (mysqli_connect_errno()){
     echo"Proses sambung ke pangkalan data gagal.";
